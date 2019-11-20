fdaf36689e58742dfacb18dd3e44e362eb696f39
ViktorSyrotynskyi <viktorsyrotynsky@gmail.com>
2019-11-20 08:12:04 +0200

    statik css img

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
+const exphbs = require(`express-handlebars`);
+const path = require(`path`);
+const nodemailer = require(`nodemailer`);
+
+const app = express();
+
+app.engine('handlebar', exphbs());
+app.set('view engine', 'handlebar');
+
+app.use('public', express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.listen(3000, () => console.log('Server started...'));
+
+const transporter = nodemailer.createTransport({
+   service: 'gmail',
+   auth: {
+       user: 'temychun@gmail.com',
+       pass: 'tamplier2287'
+   }
+});
+
+app.post('/submit', (req, res) => {
+  var mailOptions = {
+      from: req.body.email,
+      to: 'temychun@gmail.com',
+      subject: 'Send Node.js',
+      text: `Name: ${req.body.name} Email: ${req.body.email} Adress: ${req.body.adress} Phone: ${req.body.phone}` 
+  } ;
+
+    transporter.sendMail(mailOptions, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    };
+//        transporter.close();
+})
